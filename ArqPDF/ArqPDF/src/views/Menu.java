eeed7f4819dc4bbf35adf634dce0ba6096d0b68c
Vitorsdl <59091198+Vitorsdl@users.noreply.github.com>
2019-12-27 10:36:01 -0300

    Add files via upload
    Telas de menu e consulta adicionadas.

---
+++ ArqPDF/ArqPDF/src/views/Menu.java
@@ -0,0 +1,67 @@
+package views;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Menu extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Menu frame = new Menu();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Menu() {
+		super("Menu");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 451, 124);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnCadastro = new JButton("CADASTRO");
+		btnCadastro.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				CadPDF cad = new CadPDF();
+				cad.setVisible(true);
+			}
+		});
+		btnCadastro.setBounds(87, 11, 116, 53);
+		contentPane.add(btnCadastro);
+		
+		JButton btnConsulta = new JButton("CONSULTA");
+		btnConsulta.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				Consulta co = new Consulta();
+				co.setVisible(true);
+			}
+		});
+		btnConsulta.setBounds(218, 11, 116, 53);
+		contentPane.add(btnConsulta);
+	}
+}
