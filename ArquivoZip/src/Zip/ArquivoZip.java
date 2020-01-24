d46c6f0829dcd58acd471522b2eb8c77e497f2bb
danilowind <59091397+danilowind@users.noreply.github.com>
2020-01-24 17:09:36 -0300

    Add files via upload

--- ArquivoZip/src/Zip/ArquivoZip.java
+++ ArquivoZip/src/Zip/ArquivoZip.java
@@ -1,42 +1,113 @@
 package Zip;
 
-import java.io.File;
 import java.io.*;
-
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
-import java.io.IOException;
 
 public class ArquivoZip {
-	
-static final int TAMANHO_BUFFER = 1024*20; // 20kb
-    
-    public static void compactarParaZip(String arqSaida, File[] arqEntrada, int tam) throws IOException {
-        FileOutputStream destino;
-        ZipOutputStream saida;
-        try {
-            destino = new FileOutputStream(new File(arqSaida));
-            saida = new ZipOutputStream(new BufferedOutputStream(destino));
-            for (int i = 0; i < tam; i++) 
-                add(arqEntrada[i].toString(), saida);
-            saida.close();
-        } catch (IOException e) {
-            throw new IOException(e.getMessage());
-        }
-    }
 
-    private static void add(String arquivo, ZipOutputStream saida) throws IOException {
-        int cont;
-        byte[] dados = new byte[TAMANHO_BUFFER];
-        File file = new File(arquivo);
-        BufferedInputStream origem = new BufferedInputStream(new FileInputStream(file), TAMANHO_BUFFER);
-        ZipEntry entry = new ZipEntry(file.getName());
-        saida.putNextEntry(entry);
-        while ((cont = origem.read(dados, 0, TAMANHO_BUFFER)) != -1) 
-            saida.write(dados, 0, cont);
-        origem.close();
-    }
-   
+	static final int TAMANHO_BUFFER = 1024 * 20; // 20kb
+
+	public static void compactarParaZip(String arqSaida, File[] arqEntrada, int tam) throws IOException {
+		FileOutputStream destino = null;
+		ZipOutputStream saida = null;
+		try {
+			destino = new FileOutputStream(new File(arqSaida));
+			saida = new ZipOutputStream(new BufferedOutputStream(destino));
+			for (int i = 0; i < tam; i++)
+				add(arqEntrada[i].toString(), saida);
+			saida.close();
+		} catch (IOException e) {
+			throw new IOException(e.getMessage());
+		}
+	}
+
+	private static void add(String arquivo, ZipOutputStream saida) throws IOException {
+		int cont = 0;
+		byte[] dados = new byte[TAMANHO_BUFFER];
+		File file = new File(arquivo);
+		BufferedInputStream origem = new BufferedInputStream(new FileInputStream(file), TAMANHO_BUFFER);
+		ZipEntry entry = new ZipEntry(file.getName());
+		saida.putNextEntry(entry);
+		while ((cont = origem.read(dados, 0, TAMANHO_BUFFER)) != -1)
+			saida.write(dados, 0, cont);
+		origem.close();
+	}
+
+	public static void correcao(String caminho, String caminhoSalvar, int w) {
+		try {
+			 //Leitura do arquivo;
+			FileInputStream arquivo1 = new FileInputStream(caminho);
+			InputStreamReader iSR = new InputStreamReader(arquivo1);
+			BufferedReader bR = new BufferedReader(iSR);
+			// Escrita em outro arquivo;
+			FileOutputStream arquivo2 = new FileOutputStream(caminhoSalvar+"\\"+"Arquivo"+w+".txt");
+			PrintWriter pW = new PrintWriter(arquivo2);
+
+			String linha;
+			// array a são os caracteres a ser substituido; array b sao os caracteres para substituir; seguir a posicao dos dois arrays para adicao de novos caracteres;
+			char[] a = { 'À', 'Á', 'Â', 'Ã', 'Ä', 'Å', 'Æ', 'Ç', 'È', 'É', 'Ê', 'Ë', 'Ì', 'Í', 'Î', 'Ï', 'Ð', 'Ñ', 'Ò',
+					'Ó', 'Ô', 'Õ', 'Ö', 'Ù', 'Ú', 'Û', 'Ü', 'Ý', 'à', 'á', 'â', 'ã', 'ä', 'å', 'æ', 'ç', 'è', 'é', 'ê',
+					'ë', 'ì', 'í', 'î', 'ï', 'ñ', 'ò', 'ó', 'ô', 'õ', 'ö', 'ø', 'ù', 'ú', 'û', 'ü', 'ý', 'ÿ', 'Ā', 'ā',
+					'Ă', 'ă', 'Ą', 'ą', 'Ć', 'ć', 'Ĉ', 'ĉ', 'Ċ', 'ċ', 'Č', 'č', 'Ď', 'ď', 'Đ', 'đ', 'Ē', 'ē', 'Ĕ', 'ĕ',
+					'Ė', 'ė', 'Ę', 'ę', 'Ě', 'ě', 'Ĝ', 'ĝ', 'Ğ', 'ğ', 'Ġ', 'ġ', 'Ģ', 'ģ', 'Ĥ', 'ĥ', 'Ħ', 'ħ', 'Ĩ', 'ĩ',
+					'Ī', 'ī', 'Ĭ', 'ĭ', 'Į', 'į', 'İ', 'ı', 'Ĳ', 'ĳ', 'Ĵ', 'ĵ', 'Ķ', 'ķ', 'Ĺ', 'ĺ', 'Ļ', 'ļ', 'Ľ', 'ľ',
+					'Ŀ', 'ŀ', 'Ł', 'ł', 'Ń', 'ń', 'Ņ', 'ņ', 'Ň', 'ň', 'ŉ', 'Ō', 'ō', 'Ŏ', 'ŏ', 'Ő', 'ő', 'Œ', 'œ', 'Ŕ',
+					'ŕ', 'Ŗ', 'ŗ', 'Ř', 'ř', 'Ś', 'ś', 'Ŝ', 'ŝ', 'Ş', 'ş', 'Š', 'š', 'Ţ', 'ţ', 'Ť', 'ť', 'Ŧ', 'ŧ', 'Ũ',
+					'ũ', 'Ū', 'ū', 'Ŭ', 'ŭ', 'Ů', 'ů', 'Ű', 'ű', 'Ų', 'ų', 'Ŵ', 'ŵ', 'Ŷ', 'ŷ', 'Ÿ', 'Ź', 'ź', 'Ż', 'ż',
+					'Ž', 'ž', 'ſ', 'ƒ', 'Ơ', 'ơ', 'Ư', 'ư', 'Ǎ', 'ǎ', 'Ǐ', 'ǐ', 'Ǒ', 'ǒ', 'Ǔ', 'ǔ', 'Ǖ', 'ǖ', 'Ǘ', 'ǘ',
+					'Ǚ', 'ǚ', 'Ǜ', 'ǜ', 'Ǻ', 'ǻ', 'Ǽ', 'ǽ', 'Ǿ', 'ǿ', 'Ά', 'ά', 'Έ', 'έ', 'Ό', 'ό', 'Ώ', 'ώ', 'Ί', 'ί',
+					'ϊ', 'ΐ', 'Ύ', 'ύ', 'ϋ', 'ΰ', 'Ή', 'ή', '$', '²', 'ß', 'Ø', 'º', '«', '@', '', '”', 'ˆ', '‡', '±',
+					'—', 'Þ', '©', '{', '¥', '¬', '¹', '?', '›', '',
+					'' }; // Os dois ultimos estao com caracteres que nao conseguimos ver
+
+			String[] b = { "A", "A", "A", "A", "O", "A", "AE", "C", "O", "E", "E", "E", "I", "O", "I", "", "D", "O",
+					"O", "O", "O", "O", "O", "U", "U", "U", "U", "Y", "a", "a", "a", "a", "O", "a", "ae", "c", "O", "e",
+					"e", "e", "i", "O", "i", "", "O", "o", "o", "o", "o", "o", "o", "u", "u", "u", "u", "y", "y", "A",
+					"a", "A", "a", "A", "a", "C", "c", "C", "c", "C", "c", "C", "c", "D", "d", "D", "d", "E", "e", "E",
+					"e", "E", "e", "E", "e", "E", "e", "G", "g", "G", "g", "G", "g", "G", "g", "H", "h", "H", "h", "I",
+					"i", "I", "i", "I", "i", "I", "i", "I", "i", "IJ", "ij", "J", "j", "K", "k", "L", "l", "L", "l",
+					"L", "l", "L", "l", "l", "l", "N", "n", "N", "n", "N", "n", "n", "O", "o", "O", "o", "O", "o", "OE",
+					"oe", "R", "r", "R", "r", "R", "r", "S", "s", "S", "s", "S", "s", "A", "A", "T", "t", "T", "t", "T",
+					"t", "U", "u", "U", "u", "U", "u", "U", "u", "U", "u", "U", "u", "W", "w", "Y", "y", "Y", "Z", "z",
+					"Z", "z", "Z", "z", "s", "f", "O", "o", "U", "u", "A", "a", "I", "i", "O", "o", "U", "u", "U", "u",
+					"U", "u", "U", "u", "U", "u", "A", "a", "AE", "ae", "O", "o", "Α", "α", "Ε", "ε", "Ο", "ο", "Ω",
+					"ω", "Ι", "ι", "ι", "ι", "Υ", "υ", "υ", "υ", "Η", "η", "C", "I", "A", "", "C", "C", "A", "", "E",
+					"E", "", "E", "", "A", "E", "O", "I", "N", "", "/", "E", "A", "C" };
+
+			System.out.println("Executando");// Inicio da execucao;
+			do {
+				linha = bR.readLine();
+				if (linha != null) {
+					String[] palavras = linha.split("\n");
+					for (int i = 0; i < palavras.length; i++) {
+						char[] n = palavras[i].toCharArray();
+						for (int j = 0; j < n.length; j++) {
+							boolean ver = true;// verificacao para saber se escreve o caracter original ou alterado;
+							for (int k = 0; k < a.length; k++) {
+								if (n[j] == a[k]) {
+									pW.print(b[k]);
+									ver = false;
+									break;
+								}
+							}
+							if (ver) 
+								pW.print(n[j]);
+						}
+					}
+					pW.println();
+				}
+			} while (linha != null);
+			//Fechando arquivos que foram abertos
+			iSR.close();
+			pW.close();
+			arquivo1.close();
+			arquivo2.close();
+
+			System.out.println("Terminou!");// Conclusao da execucao;
+		} catch (Exception e) {
+			System.out.println("Erro ao escrever no arquivo!");
+		}
+	}
+
 }
--- ArquivoZip/src/janela/zip.java
+++ ArquivoZip/src/janela/zip.java
@@ -28,8 +28,6 @@ import javax.swing.SwingConstants;
 
 public class zip {
 	FTPClient ftp = new FTPClient();
-	Thread runner;
-	int i=0;
 	private JFrame frame;
 	private JTextField txtLogintext;
 	private JTextField txtDominiotext;
@@ -37,6 +35,7 @@ public class zip {
 	private JTextField txtDirarqtext;
 	private JTextField txtDirsalarqtext;
 	private JTextField txtLogs;
+	private JTextField txtArqCorrigido;
 
 	/**
 	 * Launch the application.
@@ -66,9 +65,9 @@ public class zip {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		
+
 		frame = new JFrame();
-		frame.setBounds(100, 100, 548, 623);
+		frame.setBounds(100, 100, 674, 623);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		JLabel statusConexaoLabel = new JLabel("Status: OFFILINE");
@@ -85,16 +84,29 @@ public class zip {
 		senhaLabel.setBounds(245, 80, 41, 14);
 
 		JLabel diretorioLabel = new JLabel("Diretorio de Arquivos: ");
-		diretorioLabel.setBounds(40, 112, 130, 14);
+		diretorioLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		diretorioLabel.setBounds(40, 112, 176, 14);
 
 		JLabel diretorioSalvarArquivosLabel = new JLabel("Diretorio Salvar Arquivo.zip: ");
-		diretorioSalvarArquivosLabel.setBounds(10, 143, 160, 14);
+		diretorioSalvarArquivosLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		diretorioSalvarArquivosLabel.setBounds(10, 143, 206, 14);
 
 		txtLogintext = new JTextField();
 		txtLogintext.setText("tempftp");
 		txtLogintext.setBounds(123, 77, 117, 20);
 		txtLogintext.setColumns(10);
 
+		txtArqCorrigido = new JTextField();
+		txtArqCorrigido.setText("C:\\\\Users\\\\m1415425\\\\Documents\\\\Novapasta2");
+		txtArqCorrigido.setBounds(226, 168, 314, 20);
+		frame.getContentPane().add(txtArqCorrigido);
+		txtArqCorrigido.setColumns(10);
+		
+		JLabel lblArqCorrigidos = new JLabel("Diretorio Salvar Arquivos Corrigidos:");
+		lblArqCorrigidos.setHorizontalAlignment(SwingConstants.CENTER);
+		lblArqCorrigidos.setBounds(10, 171, 206, 14);
+		frame.getContentPane().add(lblArqCorrigidos);
+		
 		txtDominiotext = new JTextField();
 		txtDominiotext.setBounds(113, 46, 298, 20);
 		txtDominiotext.setText("10.52.1.102");
@@ -107,7 +119,7 @@ public class zip {
 
 		txtDirarqtext = new JTextField();
 		txtDirarqtext.setEnabled(false);
-		txtDirarqtext.setBounds(169, 109, 314, 20);
+		txtDirarqtext.setBounds(226, 109, 314, 20);
 		txtDirarqtext.setText("C:\\\\Users\\\\m1415425\\\\Documents\\\\Novapasta");
 		txtDirarqtext.setColumns(10);
 
@@ -117,7 +129,7 @@ public class zip {
 
 		txtDirsalarqtext = new JTextField();
 		txtDirsalarqtext.setEnabled(false);
-		txtDirsalarqtext.setBounds(169, 140, 314, 20);
+		txtDirsalarqtext.setBounds(226, 140, 314, 20);
 		txtDirsalarqtext.setText("C:\\\\Users\\\\m1415425\\\\Documents\\\\Zip\\\\Arquivo.zip");
 		txtDirsalarqtext.setColumns(10);
 		frame.getContentPane().setLayout(null);
@@ -142,20 +154,21 @@ public class zip {
 		frame.getContentPane().add(proximaExecucaoLabel);
 
 		JLabel logsLabel = new JLabel("Salvar Logs de Envio:");
+		logsLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		logsLabel.setVerticalAlignment(SwingConstants.TOP);
-		logsLabel.setBounds(10, 174, 149, 17);
+		logsLabel.setBounds(10, 196, 206, 17);
 		frame.getContentPane().add(logsLabel);
 
 		txtLogs = new JTextField();
 		txtLogs.setText("C:\\\\Users\\\\m1415425\\\\Documents\\\\Zip\\\\");
 		txtLogs.setEnabled(false);
-		txtLogs.setBounds(169, 171, 314, 20);
+		txtLogs.setBounds(226, 193, 314, 20);
 		frame.getContentPane().add(txtLogs);
 		txtLogs.setColumns(10);
 
 		JTextArea txtArqEnviados = new JTextArea();
 		txtArqEnviados.setEditable(false);
-		txtArqEnviados.setBounds(113, 374, 314, 199);
+		txtArqEnviados.setBounds(299, 44, 314, 199);
 		frame.getContentPane().add(txtArqEnviados);
 		JScrollPane jScrollPane = new JScrollPane();
 		jScrollPane = new JScrollPane(txtArqEnviados);
@@ -176,54 +189,85 @@ public class zip {
 		enviarBotao.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				txtArqEnviados.setText("");
-				txtDirarqtext.setEnabled(false);
-				txtDirsalarqtext.setEnabled(false);
-				txtLogs.setEnabled(false);
-				enviarBotao.setEnabled(false);
-				i++;
-				// Declaração de váriaveis necessárias
-				Date date = new Date();
-				FileInputStream arqEnviar = null;
-				try {
-					arqEnviar = new FileInputStream(txtDirsalarqtext.getText().toString());
-				} catch (FileNotFoundException e3) {
-					// TODO Auto-generated catch block
-					e3.printStackTrace();
-				}
-				Calendar data = Calendar.getInstance();
-				DateFormat formatoData = new SimpleDateFormat("dd-MM-yyyy_HH-mm");
-				GregorianCalendar gc = new java.util.GregorianCalendar();
+				
 
-				File arquivos[];
+				if (txtDirarqtext.getText().trim().equals(""))// Verifica se o campo dominio está vazio
+				{
+					JOptionPane.showMessageDialog(null, "CAMPO DIRETORIO LOCALIZAÇÃO DOS ARQUIVOS VÁZIO!", "ALERTA!", JOptionPane.ERROR_MESSAGE);
+					txtDirarqtext.setBorder(BorderFactory.createLineBorder(Color.RED));
+				} else if (txtDirsalarqtext.getText().trim().equals(""))// Verifica se o campo login está vazio
+				{
+					JOptionPane.showMessageDialog(null, "CAMPO DIRETORIO SALVAR O ARQUIVO .ZIP VÁZIO!", "ALERTA!", JOptionPane.ERROR_MESSAGE);
+					txtDirsalarqtext.setBorder(BorderFactory.createLineBorder(Color.RED));
+				} else if (txtLogs.getText().trim().equals(""))// Verifica se o campo senha está vazio
+				{
+					JOptionPane.showMessageDialog(null, "CAMPO DIRETORIO SALVAR OS ARQUIVOS LOGS VÁZIO!", "ALERTA!", JOptionPane.ERROR_MESSAGE);
+					txtLogs.setBorder(BorderFactory.createLineBorder(Color.RED));
+				} else if (txtArqCorrigido.getText().trim().equals("")) {
+				JOptionPane.showMessageDialog(null, "CAMPO DIRETORIO SALVAR OS ARQUIVOS CORRIGIDOS VÁZIO!", "ALERTA!", JOptionPane.ERROR_MESSAGE);
+					txtArqCorrigido.setBorder(BorderFactory.createLineBorder(Color.RED));
+					
+				}else {
+					enviarBotao.setEnabled(false);
+					txtDirarqtext.setEnabled(false);
+					txtDirsalarqtext.setEnabled(false);
+					txtLogs.setEnabled(false);
+					txtArqCorrigido.setEnabled(false);
+					txtArqCorrigido.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+					txtLogs.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+					txtDirarqtext.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+					txtDirsalarqtext.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+					// Declaração de váriaveis necessárias
+					Date date = new Date();
+					FileInputStream arqEnviar = null;
+					Calendar data = Calendar.getInstance();
+					DateFormat formatoData = new SimpleDateFormat("dd-MM-yyyy_HH-mm");
+					GregorianCalendar gc = new java.util.GregorianCalendar();
 
-				File diretorio = new File(txtDirarqtext.getText().toString());// Pega os arquivos
-				arquivos = diretorio.listFiles();
-				for (File arquivo : arquivos)
-					txtArqEnviados.append(arquivo.getName() + "     " + arquivo.length() + " bytes"+"   "+i + "\n");
-				horarioExecucaoLabel.setText("Horário execução: " + date);
-				gc.add(Calendar.MINUTE, 15);
-				proximaExecucaoLabel.setText("Próxima execução: " + gc.getTime());
-				try {
-					ArquivoZip.compactarParaZip(txtDirsalarqtext.getText().toString(), arquivos, arquivos.length);
-					if (ftp.storeFile("Arquivo.zip", arqEnviar))
-						lblStatus.setText("Status: Enviado");
-					else
-						lblStatus.setText("Status: Não Enviado");
-				} catch (IOException e2) {
-					e2.printStackTrace();
-				}
-				File arquivoEscrita = new File(
-						txtLogs.getText().toString() + formatoData.format(data.getTime()) + "_log.txt");
-				try (FileWriter fileWrite = new FileWriter(arquivoEscrita) // arquivo para escrita
-				) {
+					File arquivos[];
+					File diretorio;
+					diretorio = new File(txtDirarqtext.getText().toString());// Pega os arquivos
+					arquivos = diretorio.listFiles();
+					//Correção
+					int i=0;
 					for (File arquivo : arquivos) {
-						fileWrite.write(arquivo.getName() + "\n");
+						ArquivoZip.correcao(arquivo.getAbsolutePath(), txtArqCorrigido.getText().toString(),i);
+						i++;
 					}
-				} catch (IOException e1) {
-					e1.printStackTrace();
+					diretorio = new File(txtArqCorrigido.getText().toString());// Pega os arquivos
+					arquivos = diretorio.listFiles();
+					for (File arquivo : arquivos)
+						txtArqEnviados.append(arquivo.getName().toString() + "     " + arquivo.length() + " bytes" + "\n");
+					try {
+						arqEnviar = new FileInputStream(txtArqCorrigido.getText().toString());
+					} catch (FileNotFoundException e3) {
+						// TODO Auto-generated catch block
+						e3.printStackTrace();
+					}
+					horarioExecucaoLabel.setText("Horário execução: " + date);
+					gc.add(Calendar.MINUTE, 15);
+					proximaExecucaoLabel.setText("Próxima execução: " + gc.getTime());
+					try {
+						ArquivoZip.compactarParaZip(txtDirsalarqtext.getText().toString(), arquivos, arquivos.length);
+						if (ftp.storeFile("Arquivo.zip", arqEnviar))
+							lblStatus.setText("Status: Enviado");
+						else
+							lblStatus.setText("Status: Não Enviado");
+					} catch (IOException e2) {
+						e2.printStackTrace();
+					}
+					File arquivoEscrita = new File(txtLogs.getText().toString() + formatoData.format(data.getTime()) + "_log.txt");
+					try (FileWriter fileWrite = new FileWriter(arquivoEscrita) // arquivo para escrita
+					) {
+						for (File arquivo : arquivos) {
+							fileWrite.write(arquivo.getName() + "\n");
+						}
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
 				}
 			}
-			
+
 		});
 		enviarBotao.setEnabled(false);
 		enviarBotao.setBounds(180, 297, 122, 23);
@@ -278,7 +322,6 @@ public class zip {
 		desconectarBotao.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
-
 					ftp.logout();
 					ftp.disconnect();
 					txtDominiotext.setEnabled(true);
@@ -288,6 +331,7 @@ public class zip {
 					txtDirarqtext.setEnabled(false);
 					txtDirsalarqtext.setEnabled(false);
 					txtLogs.setEnabled(false);
+					txtArqCorrigido.setEnabled(false);
 					desconectarBotao.setEnabled(false);
 					enviarBotao.setEnabled(false);
 					statusConexaoLabel.setText("Status: OFFILINE");
@@ -303,9 +347,8 @@ public class zip {
 		frame.getContentPane().add(conectarBotao);
 		frame.getContentPane().add(desconectarBotao);
 		frame.getContentPane().add(enviarBotao);
+		
+
 
 	}
-	
 }
-
-
